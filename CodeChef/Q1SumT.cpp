0b76c88bef5c7ed054938600ebd1091d8cd75cd2
Johan769 <remnyachizot2015@gmail.com>
2020-10-02 11:50:40 -0500

    Subiendo los de CodeChef

---
+++ CodeChef/Q1SumT.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#define db double
+#define ll long long
+#define ar array
+
+using namespace std;
+
+int a, b;
+
+void solve() {
+    cin >> a >> b;
+    cout << a + b << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while(t--)
+        solve();
+    return 0;
+}
