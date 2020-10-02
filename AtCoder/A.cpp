af64c5fd1431959939a99e41608f7ae76ce9b066
Johan769 <remnyachizot2015@gmail.com>
2020-10-02 11:46:52 -0500

    Subiendo de AtCoder

---
+++ AtCoder/A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#define ll long long
+#define db double
+#define ar array
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+    string s;
+    cin >> a >> b >> c >> s;
+    cout << a + b + c << " " << s << endl;
+    return 0; 
+}
