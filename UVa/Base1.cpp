1ce3c86a68768485348930c68d00c5159857966f
Johan769 <remnyachizot2015@gmail.com>
2020-10-02 12:06:23 -0500

    Subiendo los problemas de Uva

---
+++ UVa/Base1.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+#define db double
+#define ar array
+#define ll long long
+
+using namespace std;
+
+int a, b;
+
+void solve() {
+    cin >> a >> b;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t;
+    cin >> t;
+    while(t--)
+        solve();
+    return 0;
+}
