8514760f85becf5c32c11df49b7593dcb756f2f1
Johan769 <remnyachizot2015@gmail.com>
2020-10-02 12:15:17 -0500

    Subiendo las bases de Java

---
+++ ProgramsJava/ProgramsA.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+import static java.lang.System.out;
+
+public class ProgramsA {
+
+    static int ans;
+    private static Scanner in = new Scanner(System.in);
+
+    public static void solve() {
+
+    }
+
+    public static void print(String s) {
+        out.println(s); 
+    }
+
+    public static String str(int n) {
+        return Integer.toString(n);
+    }
+
+    public static void main(String[] args) {
+        out.println("");
+    }
+}
